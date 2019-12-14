0512c804535407caa85bd29129a5dd0d757ed7f1
Vedant Pareek <36420365+dunefro@users.noreply.github.com>
2019-12-15 00:23:34 +0530

    Update helm.js

--- on-demand-micro-services-deployment-k8s/helm.js
+++ on-demand-micro-services-deployment-k8s/helm.js
@@ -49,7 +49,7 @@ class Helm {
               releaseName: json.releaseName,
             };
           }
-          console.logs(svc);
+          console.log(svc);
           const errLog = `Install command returned unknown response: ${responseData.json}`;
           console.error(errLog);
           throw new Error(errLog);
